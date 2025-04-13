65c79100c0e7fc4d155e25d6cfc218bdd5776b64
qihuaha <alonehua8@qq.com>
2025-04-13 15:54:04 +0800

    settings.py

--- myprojects/settings.py
+++ myprojects/settings.py
@@ -16,6 +16,8 @@ import os
 
 from datetime import timedelta
 from django.conf import settings
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
